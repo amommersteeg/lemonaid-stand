fd8430b969c6a7607af7681902d29efa6de6fb35
Alex Mommersteeg <alexmommersteeg@gmail.com>
2020-09-02 21:10:01 -0400

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+tinymce.init({
+    selector: '#tinymce',
+    height: "100%",
+    scroll: true,
+    resize: false,
+    plugins: 'print preview paste importcss code searchreplace autolink directionality visualblocks visualchars fullscreen image link media template table charmap hr nonbreaking insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+    mobile: {
+        plugins: 'paste importcss code searchreplace autolink directionality visualblocks visualchars fullscreen image link media template table charmap hr nonbreaking insertdatetime advlist lists wordcount  textpattern noneditable help charmap quickbars linkchecker emoticons'
+    },
+    menu: {
+        file: { title: 'File', items: 'newdocument | preview | print ' },
+        edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace' },
+        view: { title: 'View', items: 'visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
+        insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
+        format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | blockformats | removeformat' },
+        table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
+        tools: { title: 'Tools', items: 'code wordcount' },
+        help: { title: 'Help', items: 'help' }
+    },
+    toolbar: 'undo redo | bold italic underline strikethrough removeformat| formatselect | outdent indent | numlist bullist checklist  | charmap emoticons | image link | fullscreen',
+    block_formats: 'Paragraph=p; Div =div; Blockquote=blockquote; Heading 1=h1; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6;',
+    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+    toolbar_mode: 'sliding',
+
+    // Create custom templates for regular tasks,
+    templates: [
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+    ],
+    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+
+
+
+    // Have not updated from Default Settings
+    image_advtab: true,
+    image_caption: true,
+    link_list: [
+        { title: 'My page 1', value: 'http://www.tinymce.com' },
+        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+    ],
+    image_list: [
+        { title: 'My page 1', value: 'http://www.tinymce.com' },
+        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+    ],
+    image_class_list: [
+        { title: 'None', value: '' },
+        { title: 'Some class', value: 'class-name' }
+    ],
+    importcss_append: true,
+    noneditable_noneditable_class: 'mceNonEditable',
+    
+});
+
+let codemirrorContainer = document.getElementById('codemirror');
+
+let codeEditor = CodeMirror.fromTextArea(codemirrorContainer, {
+    lineNumbers: true,
+    mode: 'htmlmixed',
+    showCursorWhenSelecting: true,
+    autoRefresh: true,
+})
+
+
+function copyText(that){
+    let panel = document.querySelector('#nav-tabContent > .active')
+    if(!(that.id == (panel.id + "-tab"))){
+        if(that.id == "nav-html-tab"){
+            let content = tinymce.get('tinymce').getContent();
+            codeEditor.getDoc().setValue(content);
+            setTimeout(function(){
+                codeEditor.refresh()
+            }, 500);
+        }else if(that.id == "nav-word-tab"){
+            let content = codeEditor.getDoc().getValue();
+            tinymce.get('tinymce').setContent(content);
+        } 
+    }
+
+}
