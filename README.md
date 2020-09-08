405b30f5d98e3254ed03af3bb48b8bd4c017d8c3
Alex Mommersteeg <alexmommersteeg@gmail.com>
2020-09-08 09:52:34 -0400

    Combined to one window

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Lemon-aid_Stand
 All-in one toolbox for courseware developers
 
-
 # Helpfully Links
 https://www.electronjs.org/docs/tutorial/first-app#installing-electron
 https://www.electronforge.io/
 https://www.ryadel.com/en/electron-jquery-bootstrap-project-add-npm/
 https://css-tricks.com/how-i-created-a-code-beautifier-in-two-days/
+https://dev.to/nicolalc/slack-gitkraken-discord-electron-loading-screen-tutorial-3k5n
 
 https://www.tutorialspoint.com/How-to-convert-the-image-into-a-base64-string-using-JavaScript
 https://blog.soshace.com/the-ultimate-guide-to-drag-and-drop-image-uploading-with-pure-javascript/
@@ -15,3 +15,12 @@ https://webaim.org/resources/contrastchecker/
 https://dev.to/alvaromontoro/building-your-own-color-contrast-checker-4j7o
 https://www.npmjs.com/package/color-contrast-checker
 https://seballot.github.io/spectrum/#toc0
+
+https://github.com/sindresorhus/electron-store
+
+
+# Libraries Used
+This code is using TinyMCE v5.4.2, Bootstrap v5.0.0-alpha, CodeMirror v5.57.0
+
+# To Do
+Custom menu/about page<
--- main.js
+++ main.js
@@ -1,21 +1,87 @@
 const { app, BrowserWindow } = require('electron')
-//const tinymce = require('./node_modules/tinymce/tinymce.min.js')
 
-function createWindow () {
+let loadingScreen;
+let mainWindow;
+
+const createLoadingScreen = () => {
+  /// create a browser window
+  loadingScreen = new BrowserWindow({
+    width: 400,
+    height: 400,
+    /// remove the window frame, so it will rendered without frames
+    frame: false,
+    /// and set the transparency to true, to remove any kind of background
+    transparent: true
+  });
+  loadingScreen.setResizable(false);
+  loadingScreen.loadFile('src/loadingscreen.html')
+  loadingScreen.on('closed', () => loadingScreen = null);
+  loadingScreen.webContents.on('did-finish-load', () => {
+    loadingScreen.show();
+  });
+}
+
+
+  
+function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      worldSafeExecuteJavaScript: true,
+      nodeIntegration: true,
+      enableRemoteModule: true
+    },
+    show: false
+    /// set show to false, the window will be visible when to loading screen will be remove
+  });
 
   // and load the index.html of the app.
-  win.loadFile('src/index.html')
+  mainWindow.loadFile('src/index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  });
+  mainWindow.webContents.on('did-finish-load', () => {
+    /// when the content has loaded, hide the loading screen and show the main window
+    if (loadingScreen) {
+      loadingScreen.close();
+    }
+    mainWindow.show();
+  });
 }
 
-app.whenReady().then(createWindow)
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createLoadingScreen();
+  /// add a little timeout for tutorial purposes, remember to remove this
+  setTimeout(() => {
+    createWindow();
+  }, 4000);
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
